b70d841c38f915d8581aa896936c133ee7abc025
adela <adeagu_ap@hotmail.com>
2024-04-29 18:08:25 -0300

    principal

--- bton.js
+++
@@ -1,9 +0,0 @@
-const campoDeFormulario= document.querySelectorAll("[required]");
-
-campoDeFormulario.forEach((campo)=>{
-    campo.addEventListener("blur",()=> verificarCampo(campo))
-});
-
-function verificarCampo(campo){
-    console.log("Cambio", campo.id);
-}
